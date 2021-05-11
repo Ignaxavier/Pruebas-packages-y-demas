bed7621305f9d22c455ad73cd83969804b44c0ed
Ignaxavier <70979777+Ignaxavier@users.noreply.github.com>
2021-05-11 18:57:24 -0300

    Snapper

---
+++ Assets/Snapper/Editor/SnapperEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Snapper))]
+public class SnapperEditor : Editor
+{
+    Snapper _snapper;
+
+    Texture2D titleBanner;
+
+    private void OnEnable()
+    {
+        _snapper = (Snapper)target;
+
+        titleBanner = Resources.Load<Texture2D>("Snapper");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        BackGround();
+        Header();
+
+        _snapper._onoff = EditorGUILayout.Toggle("On / Off", _snapper._onoff);
+        EditorGUILayout.Space();
+
+        if (_snapper._onoff)
+        {
+            RayComponets();
+        }
+    }
+
+    private void RayComponets()
+    {
+        _snapper._DirectionOfTheRay = (Snapper.dirRay)EditorGUILayout.EnumPopup("Direction of the Ray", _snapper._DirectionOfTheRay);
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Ray Distance", EditorStyles.boldLabel);
+        _snapper._rayDistance = EditorGUILayout.Slider(_snapper._rayDistance, -50f, 50f);
+        EditorGUILayout.Space();
+        _snapper._positionOffset = EditorGUILayout.FloatField("Position Offset", _snapper._positionOffset);
+    
+    }
+
+    private void Header()
+    {
+        EditorGUILayout.Space();
+        GUI.DrawTexture(GUILayoutUtility.GetRect(20, 60), titleBanner, ScaleMode.ScaleToFit);
+        EditorGUILayout.LabelField("By Ignacio Settembrini", EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+    }
+
+    private void BackGround()
+    {
+        if (_snapper._onoff)
+        {
+            EditorGUI.DrawRect(new Rect(4, 3, 5000, 207), Color.grey);
+        }
+        else
+        {
+            EditorGUI.DrawRect(new Rect(4, 3, 5000, 114), Color.grey);
+        }
+    }
+}
---
+++ Assets/Snapper/Snapper.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Snapper : MonoBehaviour
+{
+    public      float                   _rayDistance;
+    public      float                   _positionOffset;
+    public      LayerMask               _layerMask;
+    public      Transform               _objectTouched;
+    public      dirRay                  _DirectionOfTheRay;
+    public      bool                    _onoff;
+
+    public      enum                    dirRay {X, Y, Z};
+    private     RaycastHit              hit;
+    private     Vector3                 rayVector;
+    private     float                   rayVectorMultiplyer;
+
+    void Update()
+    {
+        if (_onoff)
+        {
+            VectorRay();
+            Touch();
+            Ray();
+        }
+        else
+        {
+            _objectTouched = null;
+        }
+    }
+
+
+    private void VectorRay()
+    {
+        rayVectorMultiplyer = Mathf.Clamp(_rayDistance, -1f, 1f);
+
+        switch (_DirectionOfTheRay)
+        {
+            case dirRay.X:
+
+                rayVector = Vector3.right * rayVectorMultiplyer;
+
+                break;
+
+            case dirRay.Y:
+
+                rayVector = Vector3.up * rayVectorMultiplyer;
+
+                break;
+
+            case dirRay.Z:
+
+                rayVector = Vector3.forward * rayVectorMultiplyer;
+
+                break;
+        }
+    }
+
+    private void Touch()
+    {
+        if(_objectTouched == null)
+        {
+            _objectTouched = hit.transform;
+        }
+    }
+    
+    private void Ray()
+    {
+        Debug.DrawRay(this.transform.position, rayVector, Color.red);
+
+        if (Physics.Raycast(this.transform.position, rayVector, out hit, _rayDistance, _layerMask))
+        {
+
+            Debug.Log("Toque");
+
+            switch (_DirectionOfTheRay)
+            {
+                case dirRay.X:
+
+                    transform.position = new Vector3(_objectTouched.position.x + _positionOffset, transform.position.y, transform.position.z);
+
+                    break;
+
+                case dirRay.Y:
+
+                    transform.position = new Vector3(transform.position.x, _objectTouched.position.y + _positionOffset, transform.position.z);
+
+                    break;
+
+                case dirRay.Z:
+
+                    transform.position = new Vector3(transform.position.x, transform.position.y, _objectTouched.position.z + _positionOffset);
+
+                    break;
+            }
+        }
+    }
+}
